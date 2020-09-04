de6040768572f9d6c080f103f37e48502f168622
Peter Ellis <paellis@crimson.ua.edu>
2020-09-04 12:29:42 -0500

    message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git is great!");
-        }
+            int menuInput = 0;
+            while(menuInput != 3)
+            {
+                Console.WriteLine("Please enter 1. Say hello 2. To say howdy 3. to exit");
+                try{
+                    menuInput = int.Parse(Console.ReadLine());
+                }
+                catch(Exception e){
+                    Console.WriteLine(e.Message);
+
+                }
+            }
     }
 }
